698665bbb843382ca2f7281666ee16df400027a0
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-09 17:12:53 +0300

    Setting up the haeder component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,23 @@ import SignIn from "./pages/SignIn"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
 import SignUP from "./pages/SignUp"
+import Header from "./components/Header "
 
 
 
 export default function App() {
   return (
    <Router>
+    <Header />
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/sign-in" element={<SignIn />}/>
-      <Route path="/sign-up" element={<SignUP />}/>
-      <Route path="/profile" element={<Profile />}/>
-      <Route path="/about" element={<About />}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUP />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
 
     </Routes>
+    <h1>App</h1>
    </Router>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+   <h1>Home</h1>
   )
 }
