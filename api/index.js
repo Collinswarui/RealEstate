4fc0010cec37a347966dab08ff4a18fd1a24343a
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-10 00:29:38 +0300

    Connecting the database to our backend

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to Mongo DB!")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+
 
 const app = express()
 
@@ -6,3 +22,6 @@ const app = express()
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
+
+// 
--- client/src/components/Header .jsx
+++ client/src/components/Header .jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Header () {
   return (
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
             <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className='text-slate-500'>Wakori</span>
