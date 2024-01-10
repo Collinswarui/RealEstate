edebead3b9fcc9a6bf1bfda05ed23c32ecc5c11c
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-11 01:45:50 +0300

    Use the redux state in the SingIn page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { 
+  signInFailure, 
+  signInStart, 
+  signInSuccess 
+} from '../redux/user/userSlice'
+
 
 
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const { loading, error } = useSelector((state) => state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
     setFormData({
@@ -19,7 +26,7 @@ export default function SignIn() {
   const handleSubmit = async(e) => {
     e.preventDefault()
      try {
-      setLoading(true)
+      dispatch(signInStart())
       // Connect to the backend api for signup
       const res = await fetch('/api/user/signin', 
         {
@@ -33,16 +40,13 @@ export default function SignIn() {
   
   
         if(data.success === false) {
-          setError(data.message);
-          setLoading(false);
+          dispatch(signInFailure(data.message))
           return
         }
-        setLoading(false)
-        setError(null)
+        dispatch(signInSuccess(data))
         navigate('/')
      } catch (error) {
-      setLoading(false);
-      setError(error.message)
+      dispatch(signInFailure(data.message))
      }
    
   }
