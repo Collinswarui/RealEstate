6e1afde2e020ef3c12e2d00970ac446ac30106fc
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-17 13:54:21 +0300

    functionality to delete Estates

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -148,6 +148,30 @@ const handleShowEstates = async() => {
   }
 }
 
+
+const handleListingDelete = async(listingId) => {
+  
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+    })
+  
+    const data  = await res.json()
+  
+    if(data.success === false) {
+      console.log(data.message);
+      return
+    }
+    setUserEstates((prev) =>  
+     prev.filter((listing) => listing._id !== listingId)
+    )
+  } catch (error) {
+    console.log(error.message)
+  }
+  
+}
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -250,7 +274,7 @@ const handleShowEstates = async() => {
             </Link>
 
             <div className='flex flex-col items-center font-semibold'>
-              <button className='text-red-700
+              <button onClick={() => handleListingDelete(listing._id)} className='text-red-700
               uppercase'>
                 Delete
               </button>
