e9573554d76413a754de6f989da9eca30f9541fc
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-18 16:21:36 +0300

    Created a search api route to allow users to search for estates

--- api/Controllers/listingController.js
+++ api/Controllers/listingController.js
@@ -87,9 +87,64 @@ const getListing = asyncHandler(async(req, res, next) => {
     }
 })
 
+
+// Functionality to search for estates
+const getListings = asyncHandler(async(req, res, next) => {
+    try {
+
+        const limit = parseInt(req.query.limit) || 9  
+        const startIndex = parseInt(req.query.startIndex) || 0
+       
+
+        // If offer is undefined or false search for both offers that are true or false in the database
+        let offer = req.body.offer
+        if(offer === undefined || offer === 'false') {
+            offer = {$in: [false, true]}
+        }
+
+        // If furnished is undefined or false search for both where furnished is either true or false in the database
+        let furnished = req.query.furnished
+        if(furnished === undefined || furnished === 'false') {
+            furnished = {$in: [false, true]}
+        }
+
+        let parking = req.query.parking
+        if(parking === undefined || parking === 'false') {
+            parking = {$in: [false, true]}
+        }
+
+        let type = req.query.type
+        if(type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] }
+        }
+
+        const searchTerm = req.query.searchTerm || ''
+
+        const sort = req.query.sort || 'createdAt'
+
+        const order = req.query.order || 'desc'
+
+
+        const listings = await Listing.find({
+            name: {$regex: searchTerm, $options: 'i'},
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort(
+            {[sort]: order}
+        ).limit(limit).skip(startIndex)
+
+        return res.status(200).json(listings)
+    } catch (error) {
+        next(error)
+    }
+})
+
 export {
     createListing,
     deleteListing,
     updateListing,
     getListing,
+    getListings,
 }
--- api/Routes/listingRoute.js
+++ api/Routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing, updateListing, getListing } from "../Controllers/listingController.js";
+import { createListing, deleteListing, updateListing, getListing, getListings } from "../Controllers/listingController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router()
@@ -9,6 +9,7 @@ router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.patch('/update/:id', verifyToken, updateListing)
 router.get('/get/:id', getListing)
+router.get('/get', getListings)
 
 
 export default router
