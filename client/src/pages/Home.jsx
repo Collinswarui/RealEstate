093251c4a607b5f3570742a97e2efadde02d17b0
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-21 02:34:53 +0300

    Home page edits

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,12 +50,12 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+         Home Awaits: <span className='text-slate-500'>On Your Screen,</span>
           <br />
-          place with ease
+          Awaits Your Next Perfect Abode!
         </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+        <div className='text-gray-600 text-xs sm:text-sm'>
+          Wakori Estates is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
