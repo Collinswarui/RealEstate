d50ce5684cc7f9bdfd7faad72ce433c825179312
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-21 01:58:39 +0300

    Home page UI and functionality

--- client/src/components/Header .jsx
+++ client/src/components/Header .jsx
@@ -35,7 +35,7 @@ export default function Header () {
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
-            <h1 className="font-bold text-sm sm:text-3xl flex flex-wrap">
+            <h1 className="font-bold text-sm sm:text-4xl flex flex-wrap">
             <span className='text-slate-500'>Wakori</span>
             <span className='text-slate-700'>Estates</span>
             </h1>
