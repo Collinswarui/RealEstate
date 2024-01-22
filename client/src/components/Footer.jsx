559c68ea932fae25c96fd62f957bbb12f85550c9
Collins Wanjiru <waruiwanjiru47@gmail.com>
2024-01-22 15:47:15 +0300

    Updated the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="bg-slate-700 text-white py-4">
+        <footer className="bg-slate-700 text-white  mt-6 py-4">
           <div className="container mx-auto text-center">
             <p className="text-sm font-bold">
               © {new Date().getFullYear()} WakoriEstates. All rights reserved. 
